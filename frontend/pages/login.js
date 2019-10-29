050d41084d3dbf910422537b9fb66a2fa1fb1a82
yfthu <44047538+yfthu@users.noreply.github.com>
2019-10-29 17:34:50 +0800

    增加了登录相关请求，转移

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -11,8 +11,9 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function (options) 
+  {
+    
   },
 
   /**
@@ -26,6 +27,32 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    token = this.data.extraData.token
+    console.log("得到清华人小程序发回的token:",token)
+    var app=getApp();
+    const loginUrl = `${app.globalData.backEndUrl}/auth/login`
+    console.log("得到URL：",url)
+
+    var that = this;
+    wx.request({
+      url: loginUrl,
+      method: 'post',// 请求方式
+      data: { // 想接口提交的数据
+        'token':token
+      },
+      header: {
+        'content-type': 'application/json'// 提交的数据类型
+      },
+      success(res) {  // 成功回调
+        console.log('向后端发送数据成功！', res.data);
+
+        
+      },
+      fail() { // 失败回调
+        console.log('向后端发送数据失败！');
+      }
+    })
+
 
   },
 
@@ -67,5 +94,26 @@ Page({
   kindToggle: function (e) {
     console.log("ok!!")
     wx.switchTab({"url":"view_project/view_project"})
+  },
+  onLoginPushed: function(e){
+    //  跳转到助教“清华人”小程序
+    wx.navigateToMiniProgram(    
+      {   
+        "appId": "wx9eaa87eb532c1f35",
+        "path": "pages/index/index",
+        "envVersion": "trial",
+        "extraData": 
+          {    
+            "origin": "miniapp",
+            "type": "id.tsinghua"
+          },
+          success(res) {
+            console.log("跳转到助教小程序成功！", res)
+          },
+          fail(res){
+            console.log("跳转到助教小程序失败！", res)
+          }
+      } 
+    )
   }
 })
