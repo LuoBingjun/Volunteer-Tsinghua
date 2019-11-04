6698c62f43a14cf0d9a4197704f897286e672329
LuoBingjun <luobingjun@gmail.com>
2019-11-04 15:42:45 +0800

    modify auth

--- backend/server/auth/views.py
+++ backend/server/auth/views.py
@@ -16,6 +16,7 @@ from server.utils import login_required
 class loginSerializer(serializers.Serializer):
     token = serializers.CharField(max_length=128)
 
+
 class loginView(APIView):
     def post(self, request):
         info = loginSerializer(data=request.data)
@@ -24,47 +25,61 @@ class loginView(APIView):
             userinfo = self.get_userinfo(token)
             request.session.cycle_key()
             request.session['user'] = int(userinfo['card'])
-            
+
             user = User.objects.filter(pk=userinfo['card'])
             if user.exists():
                 first_login = False
             else:
                 first_login = True
-            
+
             response = Response({
                 'first_login': first_login,
                 'name': userinfo['name'],
                 'id': userinfo['card'],
                 'department': userinfo['department']
             })
-            response['Set-Cookie'] = 'sessionid={0}; Path=/'.format(request.session.session_key)
+            response['Set-Cookie'] = 'sessionid={0}; Path=/'.format(
+                request.session.session_key)
             return response
         else:
             return Response(info.errors, status=400)
 
     def get_userinfo(self, token):
-        # response = requests.post('https://alumni-test.iterator-traits.com/fake-id-tsinghua-proxy/api/user/session/token', data={'token':token})
-        # ret = json.loads(response.text)
-        return {
-            'card': 2017111111,
-            'name': '清小华',
-            'department': '软件学院',
+        if token == 'null':
+            return {
+                'name': '清小华',
+                'card': 2017011111,
+                'department': '软件学院'
+            }
+        print(token)
+        params = {
+            'token': token
         }
+        # headers = {'content_type':'application/json'}
+        response = requests.post(
+            'https://alumni-test.iterator-traits.com/fake-id-tsinghua-proxy/api/user/session/token', json=params).json()
+        print(response)
+        userinfo = response.get('user')
+        return userinfo
+
 
 class postUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'department', 'email', 'phone']
 
+
 class getUserSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = User
         exclude = ['join_time']
+
 
 class putUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['department', 'email', 'phone']
+
 
 class userView(APIView):
     def post(self, request):
@@ -75,7 +90,8 @@ class userView(APIView):
 
     @login_required
     def put(self, request):
-        serializer = putUserSerializer(request.user, data=request.data, partial=True)
+        serializer = putUserSerializer(
+            request.user, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
