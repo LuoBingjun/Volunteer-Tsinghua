3c0c80f302ef33177654532d943f095cedab39a2
zihan ning <ningzh17@mails.tsinghua.edu.cn>
2019-11-05 17:57:00 +0800

    完善了项目详情和报名页面

--- frontend/pages/project/submit/submit.js
+++ frontend/pages/project/submit/submit.js
@@ -9,28 +9,35 @@ Page({
         "form" : [
             {
                 "text":"姓名",
-                "type":"text"
+                "required":"true",
+                "type":"text",
+                "value":""
             },{
                 "text":"献血量",
+                "required":"true",
                 "type":"radioBox",
                 "options":[
-                        {name: '100', value: '0'},
-                        {name: '200', value: '1', checked: true}
-                ]
+                        {"name": "100", "value": "1"},
+                        {"name": "200", "value": "2"}
+                ],
+                "value":""
             },{
                 "text":"献血量",
+                "required":"true",
                 "type":"checkBox",
                 "options":[
-                        {name: '100', value: '0'},
-                        {name: '200', value: '1', checked: true}
-                ]
+                        {"name": "100", "value": "1"},
+                        {"name": "200", "value": "2"}
+                ],
+                "value":""
             },{
                 "text":"献血量",
                 "type":"radioBox",
                 "options":[
-                        {name: '100', value: '0'},
-                        {name: '200', value: '1', checked: true}
-                ]
+                        {"name": "100", "value": "1"},
+                        {"name": "200", "value": "2"}
+                ],
+                "value":""
             }
         ],
     },
@@ -39,7 +46,43 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        
+        var app=getApp();
+        const getUrl = `${app.globalData.backEndUrl}/project/detail?id=${options.projectID}`
+        console.log("从project跳转到URL：",getUrl)
+        var that=this;
+        wx.request({
+            url: getUrl,
+            method: "get",
+            header: {
+                "content-type": "application/json", // 提交的数据类型
+                "cookie":app.globalData.cookies //读取cookie
+            },
+            success(res) {  // 成功回调
+                console.log("得到的数据为",res);
+                if(res.statusCode==200)
+                {
+                    that.setData({
+                        "projectID":res.projectID,
+                        "status":"not_joined",
+                        "title":res.data.title,
+                        "form":JSON.parse(res.data.form),
+                    });
+                }
+                else 
+                {
+                    wx.showModal({
+                        title: "错误",
+                        content: JSON.stringify(res.data),
+                        });
+                }
+            },
+            fail() { // 失败回调
+                wx.showModal({
+                    title: "错误",
+                    content: "无法发送数据，请检查网络状态（也有可能是我们服务器挂了）"
+                    });
+            }
+            })
     },
   
     /**
@@ -91,28 +134,27 @@ Page({
   
     },
     radioChange: function (e) {
-        if(this.data.status=='joined')return;
-        console.log('radio',e.currentTarget.id,'发生change事件，携带value值为：', e.detail.value);
+        if(this.data.status=="joined")return;
+        console.log("radio",e.currentTarget.id,"发生change事件，携带value值为：", e.detail.value);
         var new_form=this.data.form;
         var radioItems = new_form[e.currentTarget.id].options;
         for (var i = 0, len = radioItems.length; i < len; ++i) {
             radioItems[i].checked = radioItems[i].value == e.detail.value;
         }
-
+        new_form[e.currentTarget.id].value=e.detail.value;
         this.setData({
             form: new_form
         });
     },
 
     checkboxChange: function (e) {
-        if(this.data.status=='joined')return;
-        console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+        if(this.data.status=="joined")return;
+        console.log("checkbox发生change事件，携带value值为：", e.detail.value);
         var new_form=this.data.form;
         var values = e.detail.value
         var checkboxItems = new_form[e.currentTarget.id].options;
         for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
             checkboxItems[i].checked = false;
-
             for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
                 if(checkboxItems[i].value == values[j]){
                     checkboxItems[i].checked = true;
@@ -120,16 +162,40 @@ Page({
                 }
             }
         }
-
+        new_form[e.currentTarget.id].value=e.detail.value;
         this.setData({
             form: new_form
         });
     },
-
+    on_text_changed: function(e){
+        var t=e.currentTarget.id;
+        var new_form=this.data.form;
+        new_form[t].value=e.detail.value;
+        this.setData({"form":new_form})
+    },
     on_submit:function(e){
+        var index=0;
+        const form=this.data.form;
+        for(var idx in this.data.form)
+        {
+            console.log(idx,form[idx]);
+            if(form[idx].required&&!form[idx].value)
+            {
+                this.setData({"error_prompt":`第${parseInt(idx)+1}项为必填项`})
+                return;            
+            }
+        }
+
+        
+        var values=[];
+        for(var idx in this.data.form)
+        {
+            values[idx]=this.data.form[idx].value
+        }
+        console.log(values);
         wx.showToast({
-            title: '报名成功',
-            icon: 'success',
+            title: "报名成功",
+            icon: "success",
             duration: 2000
         });
         this.setData({"status":"joined"});
