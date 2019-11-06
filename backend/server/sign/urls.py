08ee8ab533c4a442253006f940a6559e05b53f05
Luo Bingjun <luobingjun@gmail.com>
2019-11-06 20:15:12 +0800

    增加查看签到项目功能

--- backend/server/sign/urls.py
+++ backend/server/sign/urls.py
@@ -2,6 +2,7 @@ from server.sign import views
 from django.urls import path
 
 urlpatterns = [
-    path(r'manage', views.manageView.as_view()),
+    path(r'project', views.projectView.as_view()),
+    path(r'list', views.listView.as_view()),
     path(r'signin', views.signinView.as_view()),
 ]
