191e64f78d696f0240f595c0f2b6d6b7605c324f
Luo Bingjun <luobingjun@gmail.com>
2019-11-17 16:32:02 +0800

    修改redis设置

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://localhost",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
