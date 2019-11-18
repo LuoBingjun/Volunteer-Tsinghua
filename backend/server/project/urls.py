2d47aeb2c1078b7a9d83b060bbdcb091696c3800
lixiaojia2017 <lixiaojia2017@163.com>
2019-11-18 23:34:40 +0800

    增加search接口

--- backend/server/project/urls.py
+++ backend/server/project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path(r'detail', views.detailView.as_view()),
     path(r'list', views.listView.as_view()),
+    path(r'search',views.searchView.as_view()),
 ]
