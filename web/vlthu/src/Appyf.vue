d44094c37e3579c2378b46e2bd9480987f77c604
yfthu <44047538+yfthu@users.noreply.github.com>
2019-11-20 18:25:57 +0800

    tmp

---
+++ web/vlthu/src/Appyf.vue
@@ -0,0 +1,32 @@
+<!-- 展示模板 -->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hellouiui></hellouiui>
+    <helloorigin/>
+  </div>
+</template>
+ 
+<script>
+// 导入组件
+import hellouiui from './components/Hello'
+import helloorigin from './components/HelloWorld'
+export default {
+  name: 'app',
+  components: {
+    hellouiui,
+    helloorigin
+  }
+}
+</script>
+<!-- 样式代码 -->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
