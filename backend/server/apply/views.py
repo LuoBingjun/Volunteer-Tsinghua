8bb2fdfe871a9be4d4c03334d180fe7a5bf6f2cb
Bingjun Luo <35619016+LuoBingjun@users.noreply.github.com>
2019-11-25 21:26:51 +0800

    Revert "增加Job需求"

--- backend/server/apply/views.py
+++ backend/server/apply/views.py
@@ -12,7 +12,7 @@ import datetime
 
 class applySerializer(serializers.Serializer):
     form = serializers.CharField(max_length=None, min_length=None, allow_blank=False, trim_whitespace=True)
-    job_id = serializers.IntegerField(max_value=None, min_value=0)
+    project_id = serializers.IntegerField(max_value=None, min_value=0)
     
 
 # 用户报名接口 需要注意：不能重复报名 返回报名编号便于前端查询报名信息
@@ -22,25 +22,23 @@ class fillformView(APIView):
         info = applySerializer(data=request.data) # 验证数据
         if info.is_valid():
             _form = info.validated_data['form']
-            job_id = info.validated_data['job_id']
-
-
+            project_id = info.validated_data['project_id']
 
             # 项目存在是否判断
-            job_set = Job.objects.filter(id=job_id, project__deadline__gt=datetime.datetime.now(), project__finished=False)
-            if job_set.exists():
-                _job = job_set[0]
+            _a_record_for_project = Project.objects.filter(id=project_id, deadline__gt=datetime.datetime.now(), finished=False)
+            if _a_record_for_project.exists():
+                _project = _a_record_for_project[0]
             else:    
-                return Response({'error':'Job not be found'}, status=404)
+                return Response({'error':'project not be found'}, status=404)
 
             # 验证重复报名 需要使用user和prject来在applyRecord中检索
-            if ApplyRecord.objects.filter(user=request.user, job__id=job_id).exists():
-                return Response({'error':'applyinfo already exists'}, status=409)
+            if ApplyRecord.objects.filter(user=request.user, project__id=project_id).exists():
+                return Response({'error':'applyinfo already exists'}, status=400)
 
             # 项目存在且从未报名,则有下列操作
 
             # 建立新数据
-            apply_record = ApplyRecord(user=request.user, job=_job, project=_job.project,
+            apply_record = ApplyRecord(user=request.user, project=_project,
                         form=_form)
             apply_record.save()
             return Response({'apply_id':apply_record.id},status=200)
@@ -60,10 +58,7 @@ class cancelapplyView(APIView):
             apply_id = info.validated_data['apply_id']  
             queryset = ApplyRecord.objects.all()
             apply_record = get_object_or_404(queryset, id=apply_id, user=request.user)
-            if apply_record.status == 'P' or apply_record.status == 'N':
-                return Response({"error":"无法取消报名"},status=406)
-            
             apply_record.delete()
-            return Response(status=200)
+            return Response(status=200)     
         else:
             return Response(info.errors, status=400) #数据格式错误
