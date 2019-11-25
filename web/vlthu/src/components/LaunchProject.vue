84daeb54ac9c3fb6cc144744f870fce917159540
yfthu <44047538+yfthu@users.noreply.github.com>
2019-11-25 21:42:17 +0800

    完成了web端问卷系统

--- web/vlthu/src/components/LaunchProject.vue
+++ web/vlthu/src/components/LaunchProject.vue
@@ -24,6 +24,58 @@
 
         <p><button @click="addNewJob" > 添加岗位</button></p>
         
+        <div id = "form">
+          <div class = "oneQuestion" v-for = "(question,index) in form">
+            <h2>问题：{{question.text}}</h2>
+            <p><button :data-index="index" @click="deleteThisQuestion" > 删除此问题</button></p>
+
+            <input type='radio' id='bitian' value='true' v-model="question.required"/>
+		        <label for='bitian'>此问题必填</label>
+		
+            <input type='radio' id='feibi' value="" v-model="question.required"/>
+            <label for='feibi'>此问题非必填</label>
+
+            <p><label>问题：</label> <input type = "text" v-model = "question.text"/></p>
+
+
+
+
+
+            <div v-if="question.type === 'text'">  
+              文本题         
+              <p><label>默认答案：</label> <input type = "text" v-model = "question.value"/></p>              
+            </div>
+
+            <div v-if="question.type === 'radioBox'">
+              <div class = "oneOption" v-for = "(option,optionIndex) in question.options">
+                <p><label>单选题选项：</label> <input type = "text" v-model = "option.name"/></p>
+                <p><button :data-index="index" data-optionIndex="optionIndex" @click="deleteThisOption" > 删除此选项</button></p>
+              </div>
+              <p><button :data-index="index" @click="addNewOption" > 添加新选项</button></p>
+            </div>
+
+            <div v-if="question.type === 'checkBox'">
+              <div class = "oneOption" v-for = "(option,optionIndex) in question.options">
+                <p><label>多选题选项：</label> <input type = "text" v-model = "option.name"/></p>
+                <p><button :data-index="index" data-optionIndex="optionIndex" @click="deleteThisOption" > 删除此选项</button></p>
+              </div>
+              <p><button :data-index="index" @click="addNewOption" > 添加新选项</button></p>
+            </div>
+
+
+            
+
+          </div>
+        </div>
+
+        <input type='radio' id='wenben' value='text' v-model="addNewQuestionType"/>
+        <label for='wenben'>文本问题</label>
+
+        <input type='radio' id='danxuan' value="radioBox" v-model="addNewQuestionType"/>
+        <label for='danxuan'>单选题</label>
+        <input type='radio' id='duoxuan' value="checkBox" v-model="addNewQuestionType"/>
+        <label for='duoxuan'>多选题</label>
+        <p><button @click="addNewQuestion" > 添加新问题</button></p>
         
         
         <p><button @click="submit" > 建立新项目</button></p>
@@ -40,6 +92,7 @@ export default {
   data () {
     return {
         submitData: undefined,
+        addNewQuestionType: "text",
         "title": "标题",
         "content": "详情",
         "requirements": "需求" ,
@@ -54,6 +107,32 @@ export default {
             "job_content":"job1content1",
             "job_require_num":250
 			    }
+        ],
+        "form":[
+          {
+              "text":"姓名",
+              "required":"true",
+              "type":"text",
+              "value":""
+          },{
+              "text":"献血量",
+              "required":"true",
+              "type":"radioBox",
+              "options":[
+                  {"name": "100"},
+                  {"name": "200"}
+              ],
+              "value":""
+          },{
+              "text":"献血量",
+              "required":"true",
+              "type":"checkBox",
+              "options":[
+                  {"name": "100"},
+                  {"name": "200"}
+              ],
+              "value":""
+          }
         ]
     }
   },
@@ -123,10 +202,65 @@ export default {
     },
     deleteThisJob(event){
         let target = event.target
-        console.log("target:",target)
         let jobIndex = target.getAttribute("data-index")
         this.jobs.splice(jobIndex, 1)
-    }
+    },
+    deleteThisQuestion(event){
+        let target = event.target
+        let questionIndex = target.getAttribute("data-index")
+        this.form.splice(questionIndex, 1)
+    },
+    deleteThisOption(event){
+        let target = event.target
+        console.log("target:",target)
+        let questionIndex = target.getAttribute("data-index")
+        let optionIndex = target.getAttribute("data-optionIndex")
+        this.form[questionIndex].options.splice(optionIndex, 1)
+    },
+    addNewQuestion(){
+        if(this.addNewQuestionType == 'text')
+        {
+            this.$data.form.push({
+            "text":"姓名",
+            "required":"true",
+            "type":"text",
+            "value":""
+            })
+        }
+        if(this.addNewQuestionType == 'radioBox')
+        {
+            this.$data.form.push({
+              "text":"献血量",
+              "required":"true",
+              "type":"radioBox",
+              "options":[
+                  {"name": "100"},
+                  {"name": "200"}
+              ],
+              "value":""
+            })
+        }
+        if(this.addNewQuestionType == 'checkBox')
+        {
+            this.$data.form.push({
+              "text":"献血量",
+              "required":"true",
+              "type":"checkBox",
+              "options":[
+                  {"name": "100"},
+                  {"name": "200"}
+              ],
+              "value":""
+            })
+        }
+    },
+    addNewOption(event){
+        let target = event.target
+        let questionIndex = target.getAttribute("data-index")
+        this.form[questionIndex].options.push({
+            "name": "1000"
+        })
+    },
   }
 }
 </script>
