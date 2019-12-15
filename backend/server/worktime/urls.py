dd9f57184573753a9bbc5b714d806d1d94272a1c
lixiaojia2017 <lixiaojia2017@163.com>
2019-12-15 11:06:12 +0800

    测试导出名单接口

--- backend/server/worktime/urls.py
+++ backend/server/worktime/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path(r'ViewWorktime', views.ViewWorktime.as_view()),
-    path(r'Export',views.export_excel),
+    path(r'Export',views.ExportView.as_view()),
     path(r'import', views.importView.as_view()),
 ]
 
