bc8061db85715d19b1f9bb400228b6410f5f01c8
yfthu <44047538+yfthu@users.noreply.github.com>
2019-12-16 23:26:41 +0800

    重写了所有页面的页面分享事件，除特定页面外，都跳转到登录页面

--- frontend/pages/comment/comment.js
+++ frontend/pages/comment/comment.js
@@ -65,7 +65,10 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function (res) {
+    return {
+        path: '/page/login/login'
+    }
 
   },
   // enterProject:function(e){
--- frontend/pages/worktimeprove/worktimeprove.js
+++ frontend/pages/worktimeprove/worktimeprove.js
@@ -72,7 +72,10 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function (res) {
+    return {
+        path: '/page/login/login'
+    }
 
-  }  
+  }
 })
